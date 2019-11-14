fea074f230838c296dab90be7954f9b6aa0899b6
Siorek, Oskar <oskar.siorek@sabre.com>
2019-11-14 23:34:44 +0100

    duzo

--- src/app/wycieczki/wycieczki.component.ts
+++ src/app/wycieczki/wycieczki.component.ts
@@ -7,9 +7,107 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WycieczkiComponent implements OnInit {
 
+  wycieczki: Wycieczka[] = generateWycieczki();
+  cheapestIndex: number = this.cheapestIndexF();
+  expensiveIndex: number = this.expensiveIndexF();
+
+  cheapestIndexF() {
+    let oldInd = 0;
+    this.wycieczki.reduce((prev, curr, ind) => {
+      let wycieczka = prev.cenaJednostkowa < curr.cenaJednostkowa ? prev : curr
+      oldInd = prev.cenaJednostkowa < curr.cenaJednostkowa ? oldInd : ind;
+      return wycieczka;
+    })
+    return oldInd;
+  }
+
+  expensiveIndexF() {
+    let oldInd = 0;
+    this.wycieczki.reduce((prev, curr, ind) => {
+      let wycieczka = prev.cenaJednostkowa > curr.cenaJednostkowa ? prev : curr
+      oldInd = prev.cenaJednostkowa > curr.cenaJednostkowa ? oldInd : ind;
+      return wycieczka;
+    })
+    return oldInd;
+  }
+
   constructor() { }
+
+  plusButton(i: number) {
+    if (this.wycieczki[i].maxIloscMiejsca - this.wycieczki[i].rezerwacje > 0) {
+      this.wycieczki[i].rezerwacje += 1;
+    }
+  }
+
+  minusButton(i: number) {
+    if (this.wycieczki[i].rezerwacje > 0) {
+      this.wycieczki[i].rezerwacje -= 1;
+    }
+  }
 
   ngOnInit() {
   }
 
+}
+
+function generateWycieczki() {
+  return [
+    {
+      nazwa: "Wycieczka1",
+      docelowyKraj: "Niemcy",
+      dataRozpoczecia: "dzis",
+      dataZakonczenia: "jutro",
+      cenaJednostkowa: 50,
+      maxIloscMiejsca: 5,
+      opisWycieczki: "Mega dobra wycieczka",
+      rezerwacje: 0,
+      link: "http://tutorialspark.com/AngularJS/Angular.png"
+    },
+    {
+      nazwa: "Wycieczka2",
+      docelowyKraj: "Hiszpania",
+      dataRozpoczecia: "jutro",
+      dataZakonczenia: "pojutrze",
+      cenaJednostkowa: 53,
+      maxIloscMiejsca: 2,
+      opisWycieczki: "Mega słaba wycieczka",
+      rezerwacje: 0,
+      link: "http://www.maximkrynica.pl/Wycieczka-objazdowa-Inne-Inne-Bawarskie-sanktuaria-zamki-9-dni,2647,520,1,sph.jpg"
+    },
+    {
+      nazwa: "Wycieczka3",
+      docelowyKraj: "Anglia",
+      dataRozpoczecia: "dzis",
+      dataZakonczenia: "jutro",
+      cenaJednostkowa: 12,
+      maxIloscMiejsca: 27,
+      opisWycieczki: "Mega dziwna wycieczka",
+      rezerwacje: 0,
+      link: "http://www.maximkrynica.pl/Wycieczka-objazdowa-Inne-Inne-Bawarskie-sanktuaria-zamki-9-dni,2647,520,1,sph.jpg"
+    }
+    ,
+    {
+      nazwa: "Wycieczka4",
+      docelowyKraj: "Anglia",
+      dataRozpoczecia: "dzis",
+      dataZakonczenia: "jutro",
+      cenaJednostkowa: 12,
+      maxIloscMiejsca: 27,
+      opisWycieczki: "Mega dziwna wycieczka",
+      rezerwacje: 11,
+      link: "http://www.maximkrynica.pl/Wycieczka-objazdowa-Inne-Inne-Bawarskie-sanktuaria-zamki-9-dni,2647,520,1,sph.jpg"
+    }
+  ]
+}
+
+interface Wycieczka {
+  nazwa: string;
+  docelowyKraj: string;
+  dataRozpoczecia: string;
+  dataZakonczenia: string;
+  cenaJednostkowa: number;
+  maxIloscMiejsca: number;
+  opisWycieczki: string;
+  link: string;
+  rezerwacje: number;
 }
