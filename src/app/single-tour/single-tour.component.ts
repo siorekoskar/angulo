9f28118dab32bf8deb9a25566eb50bbbc8760619
Siorek, Oskar <oskar.siorek@sabre.com>
2019-11-25 23:42:43 +0100

    .

--- src/app/single-tour/single-tour.component.ts
+++ src/app/single-tour/single-tour.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { ToursService } from '../wycieczki/tours.service';
 import { Wycieczka } from '../wycieczka';
+import { BasketService } from '../basket/basket.service';
 
 @Component({
   selector: 'app-single-tour',
@@ -12,14 +13,17 @@ export class SingleTourComponent implements OnInit {
 
   index: number;
   tour: Wycieczka;
+  isInBasket: boolean;
 
   constructor(
-    private route: ActivatedRoute, 
-    private toursService: ToursService) { }
+    private route: ActivatedRoute,
+    private toursService: ToursService,
+    private basketService: BasketService) { }
 
   ngOnInit() {
     this.index = Number(this.route.snapshot.paramMap.get("id"));
     this.tour = this.toursService.getProduct(this.index);
+    this.isInBasket = this.basketService.toursChosen.includes(this.tour);
   }
 
 }
