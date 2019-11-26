e44d420ad842a33e3b647d6901023d070e9760f2
Siorek, Oskar <oskar.siorek@sabre.com>
2019-11-26 22:10:47 +0100

    removed-errors

--- src/app/wycieczki/tours.service.ts
+++ src/app/wycieczki/tours.service.ts
@@ -16,18 +16,18 @@ export class ToursService {
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<Wycieczka[]> {
-    return this.http.get<Wycieczka[]>(this.toursApiUrl).pipe(map(response => response.data));
+    return this.http.get<Wycieczka[]>(this.toursApiUrl).pipe(map((response: any) => response.data));
   };
 
   getProduct(index: number): Promise<Wycieczka> {
-    return this.http.get<Wycieczka>(`${this.toursApiUrl}/${index}`).toPromise().then(response => response.data);
+    return this.http.get<Wycieczka>(`${this.toursApiUrl}/${index}`).toPromise().then((response: any) => response.data);
   };
 
   addProduct(tour: Wycieczka): Promise<Wycieczka> {
     const httpOptions = {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' })
     };
-    return this.http.post<Wycieczka>(this.toursApiUrl, tour, httpOptions).toPromise().then(response => response.data);
+    return this.http.post<Wycieczka>(this.toursApiUrl, tour, httpOptions).toPromise().then((response: any) => response.data);
   };
 
   deleteProduct(tour: Wycieczka | number): Observable<Wycieczka> {
